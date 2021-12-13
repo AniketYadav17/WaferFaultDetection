f4e562759821d26c3b4ed761853cec16d1d94391
AniketYadav17 <anikety0000@gmail.com>
2021-12-13 12:58:47 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="wafer-fault-detection",
+    version="0.0.3",
+    description="ML project",
+    author="Aniket Yadav", 
+    packages=find_packages(),
+    license="MIT"
+)
